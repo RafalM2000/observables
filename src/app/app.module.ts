40735b83c6fea91a99e972aa3bd2fc906f8fb53f
Rafal <rmf@europe.com>
2020-04-01 07:18:01 +0200

    teh new componet ExampleOne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExampleOneComponent } from './example-one/example-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleOneComponent
   ],
   imports: [
     BrowserModule
