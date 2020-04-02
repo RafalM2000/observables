95f654da9b9e2153105af033647ba17e698309d0
Rafal <rmf@europe.com>
2020-04-02 10:10:29 +0200

    nav menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ExampleOneComponent } from './example-one/example-one.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExampleTwoComponent } from './example-two/example-two.component';
+
+export const appRouter: Routes = [
+  {path: '', component: ExampleOneComponent},
+  {path: 'one', component: ExampleOneComponent},
+  {path: 'two', component: ExampleTwoComponent},
+  {path: '**', component: ExampleOneComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleOneComponent
+    ExampleOneComponent,
+    ExampleTwoComponent
   ],
   imports: [
     BrowserModule,
