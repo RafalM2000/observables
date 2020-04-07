41a85d49b29209a47d7722006ad81daea40d3371
Rafal <rmf@europe.com>
2020-04-07 08:09:46 +0200

    class Router and how to read url from the path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck{
   title1: string = 'Angular-In-Examples';
   title2: string = 'Observables, Observers i subscribe';
   title3: string = 'fromEvent, filter, map';
+
+  constructor(private currentRoute: Router) {}
+
+  ngDoCheck() {
+    switch (this.currentRoute.url) {
+      case '/one':
+        this.title3 = 'fromEvent, filter, map';
+        break;
+      case '/two':
+        this.title3 = 'valueChanges, debounceTime';
+        break;
+      default:
+        this.title3 = 'fromEvent, filter, map';
+    }
+  }
 }
