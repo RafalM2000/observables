a382333ed83faf59da32103679dbcc9be4426eb9
Rafal <rmf@europe.com>
2020-05-25 07:03:52 +0200

    new component six

---
+++ src/app/example-six/example-six.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-example-six',
+  templateUrl: './example-six.component.html',
+  styleUrls: ['./example-six.component.css']
+})
+export class ExampleSixComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
