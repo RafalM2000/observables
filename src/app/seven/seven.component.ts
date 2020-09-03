26b99e04d962fb8e125fb75f5840a306378daad4
Rafał Mytych <rfm@europe.com>
2020-09-03 06:56:09 +0200

    changes in component seven

--- src/app/seven/seven.component.ts
+++ src/app/seven/seven.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { distinctUntilKeyChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-seven',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SevenComponent implements OnInit {
 
+  person1 = {
+    name: 'Maciej',
+    age: 20
+  };
+
+  person2 = {
+    name: 'Janusz',
+    age: 21
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+    of(this.person1, this.person2).pipe(
+      distinctUntilKeyChanged('age')
+    )
+    .subscribe(data => console.log(data));
   }
 
 }
